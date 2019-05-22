033360d80f976c195759abf6c03a02c66225879f
Nguyen Hoang An <hoangannguyen26@gmail.com>
2019-05-23 00:48:11 +0700

    Scoring, Finish level

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    [Header("Explosion VFX")]
     public GameObject explosion;
     public GameObject playerExplosion;
+
+    public int scoreValue;
+
+    // Get script 'GameController'
+    private GameController gameController;
+
 
     void Start()
     {
         GameObject gameControllerObject = GameObject.FindWithTag("GameController");
         if(gameControllerObject != null)
         {
-            
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if(gameController == null)
+        {
+            Debug.Log("Cannot found 'GameController' script");
         }
     }
     void OnTriggerEnter(Collider other)
@@ -25,7 +36,9 @@ public class DestroyByContact : MonoBehaviour
         if(other.tag == "Player")
         {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
         }
+        gameController.AddScore(scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
 
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -12,27 +13,73 @@ public class GameController : MonoBehaviour
     public float spawnWait;
     public float startWait;
     public float waveWait;
+
+    [Header("UI settings")]
+    public Text m_scoreText;
+    public Text m_resetText;
+    public Text m_gameOverText;
+
+    private int m_score;
+    private bool m_reset;
+    private bool m_gameOver;
     // Start is called before the first frame update
     void Start()
     {
+        m_score = 0;
+        m_reset = false;
+        m_gameOver = false;
+        m_scoreText.text = "";
+        m_resetText.text = "";
+        m_gameOverText.text = "";
+        UpdateScoreUI();
         StartCoroutine(SpawnWaves());
-        
+
     }
 
+    void Update()
+    {
+        if (m_reset)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
     IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(startWait);
-        while(true)
+        while (true)
         {
-            for(int i = 0; i < hazardCount; i++)
+            for (int i = 0; i < hazardCount; i++)
             {
                 Vector3 spawsPosition = new Vector3(UnityEngine.Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawsPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
+                if (m_gameOver)
+                {
+                    m_resetText.text = "Press 'R' for reset";
+                    m_reset = true;
+                    break;
+                }
             }
             yield return new WaitForSeconds(waveWait);
-        }
 
+        }
+    }
+    public void AddScore(int scoreValue)
+    {
+        m_score += scoreValue;
+        UpdateScoreUI();
+    }
+    public void GameOver()
+    {
+        m_gameOverText.text = "Game Over!";
+        m_gameOver = true;
+    }
+    private void UpdateScoreUI()
+    {
+        m_scoreText.text = "Score: " + m_score;
     }
 }
